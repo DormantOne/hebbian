9a0a65550045f5754f76a5ff2e88f742e2cf7d2b
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-11-04 13:15:41 -0500

    A lot of UI progress

--- v2/index.js
+++ v2/index.js
@@ -1,6 +1,6 @@
 import TabView from "./ui/TabView.js"
 
 document.addEventListener("DOMContentLoaded",()=>{
-    new TabView("parameterTabContainer");
+    new TabView("parameterTabContainer",0,"grid").start();
 
 })
